01d13777c8ca771b69b6d7d59520efa5dca8c630
芝华塔尼欧 <jonathanlee@zhihuatioudeMBP.lan>
2018-11-30 00:27:35 +0800

    then method can return a new Promise object in right way

--- Promise/index.js
+++ Promise/index.js
@@ -2,11 +2,19 @@ function PromiseX (callback) {
     this.__queue__ = [];
     this.__res__ = void 0;
     this.__status__ = 'pending';
-    callback(this.resolve.bind(this), this.reject.bind(this)); 
+    // 如果callback是一个PromiseX的实例对象，将该对象的带调用队列加入到this对象的调用队列中
+    if(callback instanceof PromiseX) {
+        this.__queue__ = callback.__queue__;
+    }else {
+        callback(this.resolve.bind(this), this.reject.bind(this)); 
+    }
 }
 
 var handle = function ({ onSuccess, onError }) {
     if (this.__status__ === 'resolved') {
+        if(!onSuccess) {
+            return new PromiseX(this);
+        }
         try {
             this.__res__ = onSuccess(this.__res__);
             this.__status__ = 'resolved';
@@ -14,7 +22,7 @@ var handle = function ({ onSuccess, onError }) {
             this.__res__ = error;
             this.__status__ = 'rejected';
         }
-    } else {
+    } else if(this.__status__ === 'rejected') {
         try {
             this.__res__ = onError(this.__res__);
             this.__status__ = 'resolved';
@@ -26,15 +34,13 @@ var handle = function ({ onSuccess, onError }) {
 }
 
 PromiseX.prototype.resolve = function (res) {
-    var self = this;
     this.__res__ = res;
     this.__status__ = 'resolved';
     this.__queue__.forEach(handle.bind(this));
 }
 
 PromiseX.prototype.reject = function (error) {
-    var self = this;
-    self.__result = error;
+    this.__res__ = error;
     this.__status__ = 'rejected';
     this.__queue__.forEach(handle.bind(this));
 }
@@ -47,41 +53,56 @@ PromiseX.prototype.then = function(onSuccess, onError) {
     }else {
         this.__queue__.push({onSuccess, onError});
     }
-    return new PromiseX(function (...args) {args[0]()});
+    return new PromiseX(this);
 }
 
 PromiseX.prototype.catch = function(onError) {
     this.__queue__.push({onError});
-    return new PromiseX(function (...args) {args[1]()});
+    return new PromiseX(this);
 }
 
 var callback = function (resolve, reject) {
     setTimeout(function () {
         // if(Math.random() > .5)
-        resolve('Hello Promise!!!');
+        // resolve('Hello Promise!!!');
         // else
-        // reject(new Error('some error happend..'))
+        reject(new Error('some error happend..'))
     }, 1000);
 }
 
 var p = new PromiseX(callback);
-p.then(function(res) {
+p.then(function (res) {
     console.log(res);
-    return 'Hello PromiseX ---2';
-}, function(error) {
-    console.error(error);
-    throw new Error('some error happend again...')
-}).then(function(res) {
-    console.log(res);
-    throw new Error('some error happend again aaaaaa...')
 }, function (err) {
     console.error(err);
-    throw new Error('some error happend again again...')
-}).then(function(res) {
-    console.log(res);
-}, function(err) {
+    throw new Error('some error happend again ...');
+}).catch(err => {
     console.error(err);
+    return 'everything goes well.'
+}).then(function (res) {
+    console.log(res);
+}).catch(function (err) {
+    console.error(err);
+}).then(function (res) {
+    console.log(res);
 });
+// p.then(function(res) {
+//     console.log(res);
+//     return 'Hello PromiseX ---2';
+// }, function(error) {
+//     console.error(error);
+//     throw new Error('some error happend again...')
+// }).then(function(res) {
+//     console.log(res);
+//     throw new Error('some error happend again aaaaaa...')
+// }, function (err) {
+//     console.error(err);
+//     throw new Error('some error happend again again...')
+// }).then(function(res) {
+//     console.log(res);
+// }, function(err) {
+//     console.error(err);
+// });
 // 'Hello PromiseX'
 //  'Hello PromiseX --2'
 // 'some error happend again aaaaaaaaa...'
